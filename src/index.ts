8b12579dd3b0e0ab95ad363f666665001655a5c3
anthonyloera2000 <42457669+anthonyloera2000@users.noreply.github.com>
2025-02-25 08:36:14 -0800

    changed origin and fixed misspelling

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import { deleteTask, newTask, tasks, updateTask } from "./tasks/controllers";
 const app = express();
 const port = 3000;
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/tasks", tasks);
-app.post(`/task`, newTask);
+app.post(`/tasks`, newTask);
 app.put(`/tasks/:id`, updateTask);
 app.delete(`/tasks/:id`, deleteTask);
 
